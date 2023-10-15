a3f431a180d794cb41401fa6e6d025236f215415
BrightProgrammer7 <abdelilahk627@gmail.com>
2023-10-15 13:25:26 +0100

    youtube downloader

---
+++ playlist_downloader.py
@@ -0,0 +1,18 @@
+from pytube import YouTube
+from pytube import Playlist
+from pytube import streams
+
+
+playlist_link = input('Enter playlist link : ')
+
+playlist = Playlist(playlist_link)
+
+for video in playlist:
+    link = YouTube(video)
+    link.streams.get_highest_resolution().download(
+        "C:/Users/user/Videos/Youtube")
+        
+def finish():
+    print("Download Done")
+
+playlist.register_on_complete_callback(finish())
---
+++ video_downloader.py
@@ -0,0 +1,13 @@
+from pytube import YouTube
+from pytube import streams
+
+
+video_link = input('Enter video link : ')
+
+video = YouTube(video_link)
+video.streams.get_highest_resolution().download("C:/Users/user/Videos/Youtube")
+
+def finish():
+    print("Download Done")
+
+video.register_on_complete_callback(finish())
